679c4bb6efa3c1997f7999fdd44e5aab1a71e46b
Roma <romandiakun9@gmail.com>
2021-04-22 20:33:54 -0500

    Update

--- src/test/java/step_defs/AccountDataBaseValidation.java
+++ src/test/java/step_defs/AccountDataBaseValidation.java
@@ -1,6 +1,6 @@
 package step_defs;
 
-import com.sun.xml.internal.ws.util.QNameMap;
+
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.junit.Assert;
---
+++ src/test/resources/features/Andrei.feature
@@ -0,0 +1,4 @@
+Feature: Valid withdrawals
+  Background:
+    Given user goes to the main page
+    When user logs in with valid credentials
